b2f96f04ffeba9f907a9012a1c8c52324d8dbfbf
Sebastien Saunier <seb@saunier.me>
2014-02-25 14:29:17 +0100

    rails new

---
+++ README.md
@@ -0,0 +1,10 @@
+Steps followed to create this project:
+
+First create a brand new Rails project
+
+```shell
+rails new devise_example -D postgres && cd devise_example
+rake db:create
+rake db:migrate
+```
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseExample::Application.config.secret_key_base = '93afc03648b128d8dd8bc0c11c296bf5d0626fb11f2e1d47fc2052471652c80dceb53891196f0dc06f8b6774808a87f2dd25ac9ce9f743cfb794e9d94526cfd2'
